92380a78c8a70194158458244ef8d267dc4d9660
HarshBardolia01 <harshbardolia02@gmail.com>
2024-01-02 13:32:46 +0530

    basic-setup

---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { Express, Request, Response } from "express";
+const port = 8000;
+
+const app: Express = express();
+
+app.get("/hi", (req: Request, res: Response) => {
+    res.send("Hiiii!!!!");
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from index.ts");
+});
+
+app.listen(port, () => {
+    console.log(`now listening on http://localhost:${port}/`);
+});
