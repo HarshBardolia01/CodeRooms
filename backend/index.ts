6c6812bb2e2ca62971dede8ce3281d274caa6f99
HarshBardolia01 <harshbardolia02@gmail.com>
2024-01-04 13:15:13 +0530

    added-http-responses

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Logger } from "./src/util/logger";
 import { Express, Request, Response } from "express";
+import { Ok } from "./src/response/http-responses";
 const port = 8000;
 
 const app: Express = express();
@@ -10,7 +11,13 @@ app.get("/hi", (req: Request, res: Response) => {
 });
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello from index.ts");
+    return Ok(
+        res,
+        "this is from index page!",
+        {
+            testing: "hello"
+        }
+    );
 });
 
 app.listen(port, () => {
