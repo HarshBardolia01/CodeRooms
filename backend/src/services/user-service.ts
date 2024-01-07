03fc147a090c5705145a009590f0fba7ec0c0919
HarshBardolia01 <harshbardolia02@gmail.com>
2024-01-07 22:43:55 +0530

    service-todos

---
+++ backend/src/services/user-service.ts
@@ -0,0 +1,15 @@
+/**
+ * @todo
+ * 
+ * - create
+ * - getById
+ * - getByEmail
+ * - getByHandle
+ * - updateById
+ * - updateByEmail
+ * - updateByHandle
+ * - deleteById
+ * - deleteByEmail
+ * - deleteByHandle
+ * 
+ */
