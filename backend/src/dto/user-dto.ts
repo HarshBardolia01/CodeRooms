28f04f15087f61a62b3175f54f5524d88e9c6be8
HarshBardolia01 <harshbardolia02@gmail.com>
2024-01-11 11:34:01 +0530

    added users' dto

--- backend/src/dto/user-dto.ts
+++ backend/src/dto/user-dto.ts
@@ -3,3 +3,35 @@ export enum Roles {
     USER = "User",
     MEMBER = "Member"
 };
+
+export class UserDto {
+    email: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    city: string = "";
+    country: string = "";
+    organization: string = "";
+    friendOfCount: number = 0;
+    avatar: string = "";
+    about: string = "";
+    coverPhoto: string = "";
+    twitterLink: string = "";
+    linkedinLink: string = "";
+    website: string = "";
+
+    constructor(obj: any) {
+        this.email = obj && obj.email ? obj.email : "";
+        this.firstName = obj && obj.firstName ? obj.firstName : "";
+        this.lastName = obj && obj.lastName ? obj.lastName : "";
+        this.city = obj && obj.city ? obj.city : "";
+        this.country = obj && obj.country ? obj.country : "";
+        this.organization = obj && obj.organization ? obj.organization : "";
+        this.friendOfCount = obj && obj.friendOfCount ? obj.friendOfCount : "";
+        this.avatar = obj && obj.avatar ? obj.avatar : "";
+        this.about = obj && obj.about ? obj.about : "";
+        this.website = obj && obj.website ? obj.website : "";
+        this.coverPhoto = obj && obj.coverPhoto ? obj.coverPhoto : "";
+        this.twitterLink = obj && obj.twitterLink ? obj.twitterLink : "";
+        this.linkedinLink = obj && obj.linkedinLink ? obj.linkedinLink : "";
+    }
+};
